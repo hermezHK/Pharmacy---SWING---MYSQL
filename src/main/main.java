3913a785ba8a07714df567c685a30a3aa3021b45
Hermez Jaramillo <angelo_stk06@hotmail.com>
2023-12-05 18:30:41 -0500

    feat: initial config / create loginView

---
+++ src/main/main.java
@@ -0,0 +1,12 @@
+
+package main;
+
+import views.LoginView;
+
+public class main {
+    public static void main(String[] args) {
+        // Instant login
+        LoginView login = new LoginView();
+        login.setVisible(true);
+    }
+}

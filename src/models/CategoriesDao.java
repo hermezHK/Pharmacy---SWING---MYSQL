498e02b20fe55468c8720528f0fb52177c275107
Hermez Jaramillo <angelo_stk06@hotmail.com>
2023-12-17 20:26:09 -0500

    feat:  create model and methods category

---
+++ src/models/CategoriesDao.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+import java.sql.Timestamp;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author akira
+ */
+public class CategoriesDao {
+
+    //instantiate the connection
+    ConnectionMySQL cn = new ConnectionMySQL();
+    Connection conn;
+    PreparedStatement pst;
+    ResultSet rs;
+
+    //register categories
+    public boolean registerCategoryQuery(Categories category) {
+        String query = "INSERT INTO categories (name, created, updated) VALUES (?,?,?)";
+        Timestamp datetime = new Timestamp(new Date().getTime());
+
+        try {
+            conn = cn.getConnection();
+            pst = conn.prepareStatement(query);
+            pst.setString(1, category.getName());
+            pst.setTimestamp(2, datetime);
+            pst.setTimestamp(3, datetime);
+            pst.execute();
+            return true;
+
+        } catch (SQLException e) {
+            JOptionPane.showInternalMessageDialog(null, "error when registering the category");
+            return false;
+        }
+    }
+
+    //list categories
+    public List listCategoriesQuery(String value) {
+        List<Categories> list_categories = new ArrayList();
+        String query = "SELECT * FROM categories";
+        String query_search_category = "SELECT * FROM categories WHERE name LIKE '%" + value + "%'";
+
+        try {
+            conn = cn.getConnection();
+            if (value.equalsIgnoreCase("")) {
+                pst = conn.prepareStatement(query);
+                rs = pst.executeQuery();
+            } else {
+                pst = conn.prepareStatement(query_search_category);
+                rs = pst.executeQuery();
+            }
+
+            while (rs.next()) {
+                Categories category = new Categories();
+                category.setId(rs.getInt("id"));
+                category.setName(rs.getString("name"));
+                list_categories.add(category);
+            }
+
+        } catch (SQLException e) {
+            JOptionPane.showInternalMessageDialog(null, e.toString());
+        }
+        return list_categories;
+    }
+
+    //update categories
+    public boolean updateCategoryQuery(Categories category) {
+        String query = "UPDATE categories SET name = ?, updated = ? WHERE id = ?";
+        Timestamp datetime = new Timestamp(new Date().getTime());
+
+        try {
+            conn = cn.getConnection();
+            pst = conn.prepareStatement(query);
+            pst.setString(1, category.getName());
+            pst.setTimestamp(2, datetime);
+            pst.setInt(3, category.getId());
+            pst.execute();
+            return true;
+
+        } catch (SQLException e) {
+            JOptionPane.showInternalMessageDialog(null, "error updating category data");
+            return false;
+        }
+    }
+
+    //delete categories
+    public boolean deleteCategoryQuery(int id) {
+        String query = "DELETE FROM categories WHERE id = " + id;
+        try {
+            conn = cn.getConnection();
+            pst = conn.prepareStatement(query);
+            pst.execute();
+            return true;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "You cannot delete a category that has a relationship with another table");
+            return false;
+        }
+    }
+
+}

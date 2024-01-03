5d473be99a45475a5c74b75c9d1e9c999ff32b01
Hermez Jaramillo <angelo_stk06@hotmail.com>
2024-01-02 22:53:15 -0500

    feat:  modify employee data controller

--- src/controllers/EmployeesController.java
+++ src/controllers/EmployeesController.java
@@ -22,7 +22,7 @@ import views.SystemView;
  *
  * @author akira
  */
-public class EmployeesController implements ActionListener, MouseListener, KeyListener{
+public class EmployeesController implements ActionListener, MouseListener, KeyListener {
 
     private Employees employee;
     private EmployeesDao employeeDao;
@@ -39,6 +39,10 @@ public class EmployeesController implements ActionListener, MouseListener, KeyLi
 
         //buttom register employee
         this.views.btn_register_employee.addActionListener(this);
+
+        //buttom modify employee
+        this.views.btn_update_employee.addActionListener(this);
+
         this.views.employees_table.addMouseListener(this);
         this.views.txt_search_employee.addKeyListener(this);
     }
@@ -69,22 +73,60 @@ public class EmployeesController implements ActionListener, MouseListener, KeyLi
                 employee.setRol(views.cmb_rol.getSelectedItem().toString());
 
                 if (employeeDao.registerEmployeeQuery(employee)) {
+
                     cleanTable();
+                    cleanFields();
+                    listAllEmployees();
+
                     JOptionPane.showMessageDialog(null, "successfully registered employee");
                 } else {
                     JOptionPane.showMessageDialog(null, "An error occurred while registering the employee");
                 }
             }
+        } else if (e.getSource() == views.btn_update_employee) {
+            if (views.txt_employee_id.equals("")) {
+                JOptionPane.showMessageDialog(null, "select a row to continue");
+            } else {
+                //check if the fields are empty
+                if (views.txt_employee_id.getText().equals("")
+                        || views.txt_employee_fullname.getText().equals("")
+                        || views.cmb_rol.getSelectedItem().toString().equals("")) {
+
+                    JOptionPane.showMessageDialog(null, "All fields are required");
+
+                } else {
+                    employee.setId(Integer.parseInt(views.txt_employee_id.getText().trim()));
+                    employee.setFull_name(views.txt_employee_fullname.getText().trim());
+                    employee.setUsername(views.txt_employee_username.getText().trim());
+                    employee.setAddress(views.txt_employee_address.getText().trim());
+                    employee.setTelephone(views.txt_employee_telephone.getText().trim());
+                    employee.setEmail(views.txt_employee_email.getText().trim());
+                    employee.setPassword(String.valueOf(views.txt_employee_password.getPassword()));
+                    employee.setRol(views.cmb_rol.getSelectedItem().toString());
+
+                    if (employeeDao.updateEmployeeQuery(employee)) {
+                        
+                        cleanTable();
+                        cleanFields();
+                        listAllEmployees();
+                        
+                        views.btn_register_employee.setEnabled(true);
+                        JOptionPane.showMessageDialog(null, "employee data has been successfully modified");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "An error occurred while modifying the employee");
+                    }
+                }
+            }
         }
     }
-    
+
     //list all employees
-    public void listAllEmployees(){
-        if(rol.equals("Admin")){
+    public void listAllEmployees() {
+        if (rol.equals("Admin")) {
             List<Employees> list = employeeDao.listEmployeesQuery(views.txt_search_employee.getText());
             model = (DefaultTableModel) views.employees_table.getModel();
             Object[] row = new Object[7];
-            for(int i = 0; i < list.size(); i++){
+            for (int i = 0; i < list.size(); i++) {
                 row[0] = list.get(i).getId();
                 row[1] = list.get(i).getFull_name();
                 row[2] = list.get(i).getUsername();
@@ -96,13 +138,13 @@ public class EmployeesController implements ActionListener, MouseListener, KeyLi
             }
         }
     }
-    
+
     //MouseListener 
     @Override
     public void mouseClicked(MouseEvent e) {
-        if(e.getSource() == views.employees_table){
+        if (e.getSource() == views.employees_table) {
             int row = views.employees_table.rowAtPoint(e.getPoint());
-            
+
             views.txt_employee_id.setText(views.employees_table.getValueAt(row, 0).toString());
             views.txt_employee_fullname.setText(views.employees_table.getValueAt(row, 1).toString());
             views.txt_employee_username.setText(views.employees_table.getValueAt(row, 2).toString());
@@ -110,56 +152,70 @@ public class EmployeesController implements ActionListener, MouseListener, KeyLi
             views.txt_employee_telephone.setText(views.employees_table.getValueAt(row, 4).toString());
             views.txt_employee_email.setText(views.employees_table.getValueAt(row, 5).toString());
             views.cmb_rol.setSelectedItem(views.employees_table.getValueAt(row, 6).toString());
-            
+
             //disable
             views.txt_employee_id.setEditable(false);
             views.txt_employee_password.setEnabled(false);
             views.btn_register_employee.setEnabled(false);
         }
     }
-    
+
     @Override
     public void mousePressed(MouseEvent e) {
-        
+
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        
+
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
-        
+
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
-        
+
     }
-    
-    
+
     //KeyListener
     @Override
     public void keyTyped(KeyEvent e) {
-       
+
     }
-    
+
     @Override
     public void keyPressed(KeyEvent e) {
-        
+
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        if(e.getSource() == views.txt_search_employee){
+        if (e.getSource() == views.txt_search_employee) {
             cleanTable();
             listAllEmployees();
         }
     }
-    
-    public void cleanTable(){
-        for(int i = 0; i < model.getRowCount(); i++){
+
+    //cleanfields
+    public void cleanFields() {
+        views.txt_employee_id.setText("");
+        views.txt_employee_id.setEditable(true);
+        views.txt_employee_fullname.setText("");
+        views.txt_employee_username.setText("");
+        views.txt_employee_address.setText("");
+        views.txt_employee_telephone.setText("");
+        views.txt_employee_email.setText("");
+        views.txt_employee_password.setText("");
+        views.cmb_rol.setSelectedIndex(0);
+
+    }
+
+    //cleantable
+    public void cleanTable() {
+        for (int i = 0; i < model.getRowCount(); i++) {
             model.removeRow(i);
             i = i - 1;
         }
